86dea372719e5febbd7f57349723a75ba69c0888
N-Gear\masa <masahiro8218@gmail.com>
2024-10-31 16:37:13 +0900

    10/31 終了

--- components/Hero.js
+++ components/Hero.js
@@ -1,5 +1,8 @@
+'use client';
+
 import React from 'react';
 import Header from './Header';
+import ReactCountUp from 'react-countup';
 
 function Hero() {
   return (
@@ -19,13 +22,63 @@ function Hero() {
           <p className='z-20'>全国100店舗オープンを目指しています！</p>
         </div>
         {/* タイトル */}
+        <div className='flex flex-col gap-[1.5rem] text-7xl font-bold text-white'>
+          <div>
+            <span className='stroke-text'>ORIGIN</span>
+            <span>.Dog</span>
+          </div>
+          <div>
+            <span>テニススクール</span>
+          </div>
+          <div className='text-lg font-normal w-[80%] tracking-wider normal-case'>
+            <p>
+              約5,000人のお客様にご愛願いただいております。
+              会社帰りに職場の近くのテニススクールへ・休日にご自宅近くのテニススクールへ。
+            </p>
+          </div>
+        </div>
         {/* 軽い実績 */}
+        <div className='flex gap-[2rem] pt-[2rem] text-center'>
+          <div className='flex flex-col'>
+            <span className='text-white text-3xl'>
+              <ReactCountUp start={100} end={140} duration={4} />人
+            </span>
+            <p className='text-gray'>在籍コーチ</p>
+          </div>
+          <div>
+            <span className='text-white text-3xl'>
+              <ReactCountUp start={5000} end={5978} duration={4} />名
+            </span>
+            <p className='text-gray'>会員数</p>
+          </div>
+          <div>
+            <span className='text-white text-3xl'>
+              <ReactCountUp start={10} end={62} duration={4} />名
+            </span>
+            <p className='text-gray'>店舗数</p>
+          </div>
+        </div>
         {/* 案内ボタン */}
+        <div className='flex gap-[1rem] font-normal pt-[1rem]'>
+          <a
+            href='#'
+            className='text-white bg-pink w-[8rem] p-[0.5rem] font-bold text-center'
+          >
+            会員
+          </a>
+          <a
+            href='#'
+            className='text-white bg-transparent w-[8rem] p-[0.5rem] border-[4px] border-pink font-bold text-center'
+          >
+            体験
+          </a>
+        </div>
       </div>
 
       {/* 右 */}
-      <div className='col-span-4'>
+      <div className='col-span-4 relative bg-gradient-to-br from-pink via-pink to-yellow-500'>
         {/* レート */}
+        
         {/* メイン写真 */}
         {/* 背景写真 */}
       </div>
